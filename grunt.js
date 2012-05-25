af3691cf4f493009262ba2346f26d8f5de434485
Brad Gignac <bgignac@bradgignac.com>
2012-05-24 22:34:07 -0400

    Added watch task for linting changed files.

--- grunt.js
+++ grunt.js
@@ -4,7 +4,8 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: '<json:package.json>',
     meta: {
-      banner: '/* <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      banner:
+        '/* <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
         ' * <%= pkg.homepage %>\n' +
         ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
@@ -24,6 +25,10 @@ module.exports = function (grunt) {
         src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
         dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.min.js'
       }
+    },
+    watch: {
+      files: ['<config:lint.grunt>', '<config:lint.lib>', '<config:lint.spec>'],
+      tasks: 'lint'
     }
   });
 
