5a51b50eb11b5900a025c60f9558059ff67f0910
Brad Gignac <bgignac@bradgignac.com>
2012-05-24 22:41:14 -0400

    Cleaned up banner.

--- grunt.js
+++ grunt.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
     pkg: '<json:package.json>',
     meta: {
       banner:
-        '/* <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '/* <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
         ' * <%= pkg.homepage %>\n' +
         ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
